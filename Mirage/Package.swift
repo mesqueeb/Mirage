615d1dbe3c0f96452d190e36e72af68cc560e90b
mesqueeb <mesqueeb@users.noreply.github.com>
2024-11-19 00:15:07 -0800

    chore: remove unused dependencies

--- Mirage/Package.swift
+++ Mirage/Package.swift
@@ -7,14 +7,10 @@ let package = Package(
   name: "Mirage",
   platforms: [.macOS(.v15), .iOS(.v18), .visionOS(.v2)],
   products: [.library(name: "Mirage", targets: ["Mirage"])],
-  dependencies: [
-    .package(url: "https://github.com/mesqueeb/JustSugar", from: "0.2.2"),
-    .package(url: "https://github.com/mesqueeb/MapSugar", from: "0.1.0"),
-  ],
+  dependencies: [],
   targets: [
     .target(
       name: "Mirage",
-      dependencies: ["JustSugar", "MapSugar"],
       path: "Sources",
       swiftSettings: [.enableExperimentalFeature("StrictConcurrency")]
     )
