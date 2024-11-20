252fb72a510a870cfbe287575654796f00999ee0
mesqueeb <mesqueeb@users.noreply.github.com>
2024-11-19 22:57:12 -0800

    fix: cleanup

--- Mirage/Sources/Styles/Typography.swift
+++ Mirage/Sources/Styles/Typography.swift
@@ -1,60 +1,47 @@
 import SwiftUI
 
-#Preview {
+#Preview(traits: .sizeThatFitsLayout) {
   VStack(spacing: Space.md) {
     #if os(visionOS)
-      VStack {
-        Text("extraLargeTitle").font(.extraLargeTitle)
-        Text("Create a font with the extra large title text style.")
-      }
-      VStack {
-        Text("extraLargeTitle2").font(.extraLargeTitle2)
-        Text("Create a font with the second level extra large title text style.")
-      }
+      VStack { Text("extraLargeTitle").font(.extraLargeTitle) }
+      VStack { Text("extraLargeTitle2").font(.extraLargeTitle2) }
     #endif
-    VStack {
-      Text("largeTitle").font(.largeTitle)
-      Text("A font with the large title text style.")
-    }
-    VStack {
-      Text("title").font(.title)
-      Text("A font with the title text style.")
-    }
-    VStack {
-      Text("title2").font(.title2)
-      Text("Create a font for second level hierarchical headings.")
-    }
-    VStack {
-      Text("title3").font(.title3)
-      Text("Create a font for third level hierarchical headings.")
-    }
+    VStack { Text("largeTitle").font(.largeTitle) }
+    VStack { Text("title").font(.title) }
+    VStack { Text("title2").font(.title2) }
+    VStack { Text("title3").font(.title3) }
     VStack {
       Text("headline").font(.headline)
-      Text("A font with the headline text style.")
+      Text("A font with the headline text style.").font(.headline)
     }
     VStack {
       Text("subheadline").font(.subheadline)
-      Text("A font with the subheadline text style.")
+      Text("A font with the subheadline text style.").font(.subheadline)
     }
     VStack {
       Text("body").font(.body)
-      Text("A font with the body text style.")
+      Text("A font with the body text style.").font(.body)
     }
     VStack {
       Text("callout").font(.callout)
-      Text("A font with the callout text style.")
+      Text("A font with the callout text style.").font(.callout)
     }
     VStack {
       Text("caption").font(.caption)
-      Text("A font with the caption text style.")
+      Text("A font with the caption text style.").font(.caption)
     }
     VStack {
       Text("caption2").font(.caption2)
-      Text("Create a font with the alternate caption text style.")
+      Text("Create a font with the alternate caption text style.").font(.caption2)
     }
     VStack {
       Text("footnote").font(.footnote)
-      Text("A font with the footnote text style.")
+      Text("A font with the footnote text style.").font(.footnote)
     }
   }
+  .padding()
+
+  #if os(visionOS)
+    .glassBackgroundEffect()
+  #endif
 }
