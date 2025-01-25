4200a0a18ad79e3bfe11b1cbd71ecaada92483b5
Luca Ban <mesqueeb@users.noreply.github.com>
2025-01-25 14:09:16 +1300

    feat(MHorizontalRule): add height prop, make default maxWidth be `.infinity` (like 100% in css)

--- Mirage/Sources/Components/MHorizontalRule.Examples.swift
+++ Mirage/Sources/Components/MHorizontalRule.Examples.swift
@@ -14,27 +14,75 @@ public struct MHorizontalRule_Examples: View {
 
   public var body: some View {
     ScrollView {
-      Grid(horizontalSpacing: 20, verticalSpacing: 20) {
-        GridRow {
-          exampleCell(text: "Default HR", hr: MHorizontalRule())
-          exampleCell(text: "Thick Line", hr: MHorizontalRule(height: 5))
-          exampleCell(text: "Thin Line", hr: MHorizontalRule(height: 0.5))
+      VStack {
+        Grid(horizontalSpacing: 20, verticalSpacing: 20) {
+          GridRow {
+            exampleCell(text: "Default HR", hr: MHorizontalRule())
+            exampleCell(text: "Thick Line", hr: MHorizontalRule(height: 5))
+            exampleCell(text: "Thin Line", hr: MHorizontalRule(height: 0.5))
+          }
+          GridRow {
+            exampleCell(text: "Red HR", hr: MHorizontalRule(color: .red))
+            exampleCell(text: "Wide HR", hr: MHorizontalRule(width: 100))
+            exampleCell(text: "Narrow HR", hr: MHorizontalRule(width: 30))
+          }
+          GridRow {
+            exampleCell(text: "Custom Opacity", hr: MHorizontalRule(color: Color.blue.opacity(0.6)))
+            exampleCell(
+              text: "Rounded HR",
+              hr: MHorizontalRule(color: .green, width: 80, height: 3)
+            )
+            exampleCell(
+              text: "Custom Combo",
+              hr: MHorizontalRule(color: .orange, width: 70, height: 4)
+            )
+          }
         }
-        GridRow {
-          exampleCell(text: "Red HR", hr: MHorizontalRule(color: .red))
-          exampleCell(text: "Wide HR", hr: MHorizontalRule(width: 100))
-          exampleCell(text: "Narrow HR", hr: MHorizontalRule(width: 30))
+
+        MHorizontalRule(width: .infinity)
+
+        Text("Width")
+
+        VStack(spacing: Space.md) {
+          Text("this HR will have its default width applied")
+          MHorizontalRule()
+          Text("this HR will be 500px wide")
+          MHorizontalRule(width: 500)
+          Text("this HR will grow until its parent frame")
+          MHorizontalRule(width: .infinity)
+          Text("That's all")
         }
-        GridRow {
-          exampleCell(text: "Custom Opacity", hr: MHorizontalRule(color: Color.blue.opacity(0.6)))
-          exampleCell(text: "Rounded HR", hr: MHorizontalRule(color: .green, width: 80, height: 3))
-          exampleCell(
-            text: "Custom Combo",
-            hr: MHorizontalRule(color: .orange, width: 70, height: 4)
-          )
+        .frame(width: 600).border(.brown, width: 1).padding()
+
+        VStack(spacing: Space.md) {
+          Text("this HR will have its default width applied")
+          MHorizontalRule()
+          Text("this HR will be 500px wide")
+          MHorizontalRule(width: 500)
+          Text("this HR will grow until its parent frame")
+          MHorizontalRule(width: .infinity)
+          Text("That's all")
         }
+        .frame(width: 70).border(.brown, width: 1).padding()
+
+        MHorizontalRule(width: .infinity)
+
+        Text("Height")
+
+        HStack(spacing: Space.md) {
+          MHorizontalRule()
+          MHorizontalRule(height: 40)
+          MHorizontalRule(height: .infinity)
+        }
+        .frame(height: 60).border(.brown, width: 1).padding()
+
+        HStack(spacing: Space.md) {
+          MHorizontalRule()
+          MHorizontalRule(height: 40)
+          MHorizontalRule(height: .infinity)
+        }
+        .frame(height: 10).border(.brown, width: 1).padding()
       }
-      .padding()
     }
   }
 
--- Mirage/Sources/Components/MHorizontalRule.swift
+++ Mirage/Sources/Components/MHorizontalRule.swift
@@ -1,18 +1,45 @@
 import SwiftUI
 
+@MainActor extension View {
+  @ViewBuilder fileprivate func applyHrFrame(width: CGFloat, height: CGFloat) -> some View {
+    // Compute (minWidth, maxWidth)
+    let (minWidth, maxWidth): (CGFloat, CGFloat) =
+      width.isInfinite ? (0, .infinity) : (width, width)
+    // Compute (minHeight, maxHeight)
+    let (minHeight, maxHeight): (CGFloat, CGFloat) =
+      height.isInfinite ? (0, .infinity) : (height, height)
+    // Finally, apply all at once
+    self.frame(
+      minWidth: minWidth,
+      maxWidth: maxWidth,
+      minHeight: minHeight,
+      maxHeight: maxHeight,
+      alignment: .center
+    )
+  }
+}
+
+/// A horizontal line.
+///
+/// By default it will be 1px high and grow horizontally as wide as the parent frame.
+/// You can also pass a `width` and/or `height`
 public struct MHorizontalRule: View {
   private let color: Color
   private let width: CGFloat
   private let height: CGFloat
 
-  public init(color: Color = Color.primary.opacity(0.4), width: CGFloat = 50, height: CGFloat = 1) {
+  public init(
+    color: Color = Color.primary.opacity(0.4),
+    width: CGFloat = .infinity,
+    height: CGFloat = 1
+  ) {
     self.color = color
     self.width = width
     self.height = height
   }
 
   public var body: some View {
-    RoundedRectangle(cornerRadius: Space.sm).frame(width: width, height: height)
+    RoundedRectangle(cornerRadius: Space.sm).applyHrFrame(width: width, height: height)
       .foregroundColor(color)
   }
 }
